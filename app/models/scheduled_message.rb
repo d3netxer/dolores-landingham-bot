4d6752ef8d38ae50b26724716565612bdddd5c5d
Carlo Costino <carlo.costino@gsa.gov>
2015-09-25 16:34:04 -0400

    Fixes #30: Vastly improved escaping implementation.
    After some discussion it was determined that we should just be escaping the message body when it is being sent to Slack itself, not before it is saved into the database.  This change eliminates all of those steps and adds a public method to the MessageSender class that takes care of this.

--- app/models/scheduled_message.rb
+++ app/models/scheduled_message.rb
@@ -2,18 +2,4 @@ class ScheduledMessage < ActiveRecord::Base
   validates :body, presence: true
   validates :days_after_start, presence: true
   validates :title, presence: true
-
-  before_save :escape_specific_characters
-
-  def body
-    self[:body].html_safe if self[:body]
-  end
-
-  protected
-
-  def escape_specific_characters
-    self.body = self[:body].gsub('&', '&amp;')
-    self.body = self[:body].gsub('<', '&lt;')
-    self.body = self[:body].gsub('>', '&gt;')
-  end
 end
