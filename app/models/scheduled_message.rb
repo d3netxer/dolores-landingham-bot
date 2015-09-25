e0840e6a1341b74948f44a06cc706760cb0b11e1
Carlo Costino <carlo.costino@gsa.gov>
2015-09-25 12:05:55 -0400

    Fixes #30:  Escapes all special characters to their HTML entities.
    This will escape all special characters to their corresponding HTML entities,
    but it will also display the messages normally when viewing/editing them.

--- app/models/scheduled_message.rb
+++ app/models/scheduled_message.rb
@@ -2,4 +2,16 @@ class ScheduledMessage < ActiveRecord::Base
   validates :body, presence: true
   validates :days_after_start, presence: true
   validates :title, presence: true
+
+  before_save :escape_special_characters
+
+  def body
+    self[:body].html_safe
+  end
+
+  protected
+
+  def escape_special_characters
+    self.body = ERB::Util::html_escape(self[:body])
+  end
 end
